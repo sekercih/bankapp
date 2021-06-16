7067bada1f6adfef965e69bcf839f24285e3e3d7
sekercih <hkkskrc@gmail.com>
2021-06-16 23:39:40 +0300

    navbar

--- src/header/Header.js
+++ src/header/Header.js
@@ -4,47 +4,59 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-// import IconButton from "@material-ui/core/IconButton";
-// import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "react-router-dom";
 import logo from "../images/logo.png";
-
+import "./Header.css";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    color: "white",
   },
   menuButton: {
     marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
   },
   button: {
     color: "white",
     fontSize: "15px",
     fontWeight: "bold",
   },
+  title: {
+    flexGrow: 1,
+  },
+  spacer: {
+    paddingLeft: "300px",
+  },
 }));
-
 export default function Header() {
   const classes = useStyles();
-
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar variant="dense">
           <Link to="/">
-            <img src={logo} alt="" />
+            <img src={logo} alt="logo" />
           </Link>
           <Typography variant="h6" className={classes.title}>
-            MTD Bank
+            MTD BANK
           </Typography>
-          <Button className={classes.button} color="inherit">
-            ABOUT AS
-          </Button>
-          <Button className={classes.button} color="inherit">
-            PRICE
-          </Button>
+          <Button className={classes.button}>ABOUT US</Button>
+          <Button className={classes.button}>PRODUCT</Button>
+          <Button className={classes.button}>PACKAGE</Button>
+          <Button className={classes.button}>CONTACT</Button>
+          <Button className={classes.button}>PACKAGE</Button>
+          <div className={classes.spacer}></div>
+          <Link to="/login" className="header__link">
+            <div className="header__option">
+              <span className="header__lineOne">Hello</span>
+              <span className="header__lineTwo">Sign In</span>
+            </div>
+          </Link>
+          <Link to="/register" className="header__link">
+            <div className="header__option">
+              <span className="header__lineOne">New User</span>
+              <span className="header__lineTwo">Register</span>
+            </div>
+          </Link>
         </Toolbar>
       </AppBar>
     </div>
